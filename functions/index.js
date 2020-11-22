940032fd405a1ed953a8233258d955cf29274636
Alex Menor <alex4menor@gmail.com>
2020-11-22 12:39:48 +0100

    adds send feedback function

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,6 @@
 const { getTrendingWatchables } = require("./get_trending_watchables");
+const { sendFeedback } = require("./send_feedback");
 
 exports.getTrendingWatchables = getTrendingWatchables;
+
+exports.sendFeedback = sendFeedback;
---
+++ functions/send_feedback.js
@@ -0,0 +1,43 @@
+const functions = require("firebase-functions");
+const https = require("https");
+const badWords = require("../bad-words");
+
+const CHANNEL_NAME = "@sugerenciasQueVemos";
+
+exports.sendFeedback = functions.https.onRequest((request, response) => {
+  const { feedback } = request.body;
+
+  if (!feedback)
+    return response
+      .status(400)
+      .send("Could not find a feedback field in the request body");
+
+  if (isOffensive(feedback))
+    return response.status(400).send("Your feedback is not useful");
+
+  sendMsgToTelegram(feedback)
+    .then(() => {
+      return response.send({ msg: "Feedback sent" });
+    })
+    .catch(() => {
+      return response.status(500).send("Something went wrong");
+    });
+});
+
+function sendMsgToTelegram(msg) {
+  return new Promise((resolve, reject) => {
+    https.get(
+      `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_KEY}/sendMessage?chat_id=${CHANNEL_NAME}&text=${msg}`,
+      (response) => {
+        if (response.statusCode !== 200) reject(new Error());
+        else resolve();
+      }
+    );
+  });
+}
+
+function isOffensive(msg) {
+  const msgWords = msg.split(" ");
+
+  return msgWords.some((word) => badWords.includes(word));
+}
